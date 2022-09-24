f48cae4d8e4dba7ad1771e1cc0f579982147eb1b
emir <alptekine2@gmail.com>
2022-09-24 12:10:52 +0300

    added usermanager and wordmanager

--- main.py
+++ main.py
@@ -1,6 +1,26 @@
+from re import I
+import words_manager
+import user_manager
 from datetime import datetime
 from os import path
+from time import sleep
 
-def start():
-    
-    input()
+def start(username):
+    user_manager.userlogin(username)
+    userdb = open("userdb.txt" , "r" )
+    for i in userdb:
+        if username in i:
+            username , language, word_amount = i.split()
+            word_amount = int(word_amount)
+            print(username , language, word_amount)
+    words = words_manager.get_words(word_amount,language)
+    print("   ",*words)
+    sleep(1)
+    start_time = datetime.now()
+    userinput = input("GO!!")
+    finish_time = datetime.now()
+
+
+if __name__ == '__main__':
+    username = input("enter your username ")
+    start(username)
